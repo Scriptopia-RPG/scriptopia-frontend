efc1867533ea36d209c9452a0e19328e56bf3309
Dabin Jeong <allempti@gmail.com>
2025-08-21 15:10:42 +0900

    feat: MSW 세팅 및 서버 구축

--- src/shared/api/mocks/handlers.ts
+++ src/shared/api/mocks/handlers.ts
@@ -1,11 +1,10 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://api.example.com/user', () => {
+  http.get('/test', () => {
     return HttpResponse.json({
-      id: 'abc-123',
-      firstName: 'John',
-      lastName: 'Maverick',
+      postId: 1,
+      content: `${1} Stop following me. I'm too famous.`,
     });
   }),
 ];
