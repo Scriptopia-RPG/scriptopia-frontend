b360727c5e69a2d2b83f4d4adf2edc043bcba541
allkong <allempti@gmail.com>
2025-08-23 21:08:44 +0900

    chore: storybook에 프리텐다드 폰트 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,16 @@
 import type { Preview } from '@storybook/nextjs';
 import '../app/globals.css';
+import { pretendard } from '../src/shared/styles/fonts';
 
 const preview: Preview = {
   parameters: {
+    nextjs: { appDirectory: true },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
-
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
@@ -17,6 +18,14 @@ const preview: Preview = {
       test: 'todo',
     },
   },
+  decorators: [
+    (Story) => (
+      // body에 붙였던 것처럼 폰트 클래스/변수를 래퍼에 적용
+      <div style={pretendard.style}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
