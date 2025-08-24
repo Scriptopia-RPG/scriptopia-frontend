d3245a129ba9aea8810a04648329534ba0d8f955
allkong <allempti@gmail.com>
2025-08-25 04:14:13 +0900

    feat: explore 페이지 레이아웃 정리

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -2,11 +2,9 @@
 
 import { useEffect, useState } from 'react';
 
-import SearchBar from '@/shared/ui/search-bar/search-bar';
-import TagAddButton from '@/entities/tag/ui/tag-add-button';
-import Tag from '@/entities/tag/ui/tag';
-import ResetButton from '@/entities/tag/ui/reset-button';
 import Header from '@/widgets/header/ui/header';
+import SearchBar from '@/shared/ui/search-bar/search-bar';
+import TagFilter from '@/features/search-query/ui/tag-filter/ui/tag-filter';
 
 const Page = () => {
   const [q, setQ] = useState('');
@@ -23,45 +21,13 @@ const Page = () => {
     console.log(debounced);
   }, [debounced]);
 
-  const tags = [
-    { tagId: 0, tagName: '로맨스' },
-    { tagId: 1, tagName: '공포' },
-    { tagId: 2, tagName: '추리' },
-    { tagId: 3, tagName: '시뮬레이션' },
-    { tagId: 4, tagName: '아포칼립스' },
-  ];
-
-  const [selectedTags, setSelectedTags] = useState(tags);
-
   return (
     <>
       <Header />
       <div className="mx-auto mt-12 flex w-full max-w-5xl flex-col gap-7 px-4">
         <div className="flex flex-col gap-5">
           <SearchBar value={q} onChange={setQ} onClear={() => setQ('')} />
-          <div className="flex gap-2.5">
-            <div className="shrink-0">
-              <TagAddButton onClick={() => {}} />
-            </div>
-            <div className="min-w-0 flex-1 overflow-x-auto">
-              <div className="flex gap-2.5 whitespace-nowrap">
-                {selectedTags.map((tag) => (
-                  <Tag
-                    key={tag.tagId}
-                    name={tag.tagName}
-                    selected
-                    removable
-                    onRemove={() =>
-                      setSelectedTags((prev) => prev.filter((t) => t.tagId !== tag.tagId))
-                    }
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="shrink-0">
-              <ResetButton onClick={() => setSelectedTags([])} />
-            </div>
-          </div>
+          <TagFilter />
         </div>
         <div>정렬</div>
         <div>카드 리스트</div>
