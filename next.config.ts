26cf189192f62bb9fb9f1fe9abd20e26836b1fa8
allkong <allempti@gmail.com>
2025-08-25 00:46:42 +0900

    chore: next.config.ts 최신 방식으로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,35 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // TurboPack 설정
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    // SVGR 로더 (Turbopack용)
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: { typescript: true, ext: 'tsx' },
+          },
+        ],
+        as: '*.js',
       },
     },
+    resolveAlias: { '@': './src', '@icons': './src/shared/icons' },
+    resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
-  // webpack 설정
+
+  // (선택) Webpack 모드에서만 쓰이는 설정: Turbopack 모드에선 무시됨
   webpack: (config) => {
     // @ts-expect-error 타입 에러 무시
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
     config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
+      { ...fileLoaderRule, test: /\.svg$/i, resourceQuery: /url/ },
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              ext: 'tsx',
-            },
-          },
-        ],
+        use: [{ loader: '@svgr/webpack', options: { typescript: true, ext: 'tsx' } }],
       },
     );
     fileLoaderRule.exclude = /\.svg$/i;
