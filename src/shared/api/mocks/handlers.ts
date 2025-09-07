4ace7179448e4a2f2809577d658327bd046c3e01
ttotto0421 <ttotto0421@gmail.com>
2025-09-08 00:40:06 +0900

    remove: 필요없는 파일 삭제

--- src/shared/api/mocks/handlers.ts
+++ src/shared/api/mocks/handlers.ts
@@ -1,9 +1,7 @@
 import { sharedGame } from '@/shared/api/handlers/shared-game.handler';
 import { authHandlers } from '@/shared/api/handlers/auth.handler';
-import { testHandlers } from '@/shared/api/handlers/test.handler';
 
 export const handlers = [
   ...sharedGame,
   ...authHandlers,
-  ...testHandlers,
 ]
