c323ad2dfe647ef48bb465038f5496b51bf65e8b
Dabin Jeong <allempti@gmail.com>
2025-09-07 18:49:05 +0900

    feat: Modal 베이스 컴포넌트 분리

---
+++ src/shared/ui/modal/modal.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+
+interface ModalProps {
+  ariaLabelledby?: string;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ ariaLabelledby, onClose, children }: ModalProps) => {
+  return (
+    <div
+      role="dialog"
+      aria-modal
+      aria-labelledby={ariaLabelledby}
+      onClick={onClose}
+      className="fixed inset-0 z-50 flex items-center justify-center"
+    >
+      <div className="absolute inset-0 bg-black/80"></div>
+
+      <div onClick={(e) => e.stopPropagation()} className="bg-bg relative z-40 rounded-2xl">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
