1c015d593903a42d2303e0828c94a938913c05ff
ttotto0421 <ttotto0421@gmail.com>
2025-09-08 00:13:17 +0900

    refactor: 파일명 변경

--- src/features/auth/login/ui/login-modal.tsx
+++ src/features/auth/login/ui/login-modal.tsx
@@ -4,9 +4,9 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { tv, VariantProps } from "tailwind-variants";
-import { Button } from "@/shared/ui/button/button";
+import { Button } from "@/shared/ui/button/login-button";
 import Icon from "@/shared/ui/icon/icon";
-import Input from "@/shared/ui/input/input";
+import Input from "@/shared/ui/Input/input";
 import Logo from "@/shared/ui/logo/logo";
 
 const loginModalVariants = tv({
