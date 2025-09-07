6a1b424c74725e88af9df1efc8ebc16d8251d1dd
Dabin Jeong <allempti@gmail.com>
2025-09-07 16:05:36 +0900

    refactor: 모달 트리거임을 aria로 명시

--- src/entities/shared-game/ui/tag/tag-add-button.tsx
+++ src/entities/shared-game/ui/tag/tag-add-button.tsx
@@ -10,6 +10,7 @@ const TagAddButton = ({ onClick }: TagAddButtonProps) => {
   return (
     <button
       type="button"
+      aria-haspopup="dialog"
       onClick={onClick}
       className="inline-flex cursor-pointer items-center gap-1 rounded-full px-2.5 py-1 text-sm ring-1 ring-gray-200 select-none ring-inset sm:px-4 sm:py-2"
     >
