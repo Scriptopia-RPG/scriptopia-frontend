5094658ae9636d5f1aa6bea5c682bd9b58587d82
allkong <allempti@gmail.com>
2025-09-16 18:35:33 +0900

    feat: 로그인 api 호출하기

---
+++ src/entities/user/api/use-login.mutation.ts
@@ -0,0 +1,32 @@
+import useAuthStore from '@/entities/auth/model/auth.store';
+import customFetch from '@/shared/api/custom-fetch';
+import { useMutation } from '@tanstack/react-query';
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  accessToken: string;
+  expiresIn: number;
+  role: 'USER' | 'ADMIN';
+}
+
+const login = async (loginData: LoginRequest) => {
+  return await customFetch<LoginResponse>('/auth/login', {
+    method: 'POST',
+    body: loginData,
+  });
+};
+
+export const useLogin = () => {
+  const { setAuth } = useAuthStore.getState();
+  return useMutation({
+    mutationFn: login,
+    retry: false,
+    onSuccess: (res) => {
+      setAuth(res.accessToken, res.expiresIn);
+    },
+  });
+};
