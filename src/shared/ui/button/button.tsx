14b22b4cf59e3394661f6d4007ddf741ec2f5a91
allkong <allempti@gmail.com>
2025-09-16 18:34:35 +0900

    feat: Button 컴포넌트에 disabled 추가

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -5,15 +5,17 @@ import { MouseEventHandler } from 'react';
 interface ButtonProps {
   type?: 'button' | 'submit';
   label: string;
+  disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({ type = 'button', label, onClick }: ButtonProps) => {
+const Button = ({ type = 'button', label, disabled = false, onClick }: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="bg-gradient-primary w-full cursor-pointer rounded-xl py-2.5 text-white"
+      disabled={disabled}
+      className="bg-gradient-primary w-full cursor-pointer rounded-xl py-2.5 text-white disabled:bg-gray-200 disabled:bg-none"
     >
       {label}
     </button>
