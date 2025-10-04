ab4d4502b74d7b69d600d3c6f3bea9268c752e79
allkong <allempti@gmail.com>
2025-10-05 06:35:44 +0900

    feat: 공유 게임 상세 페이지에서 모달 부르기

--- app/shared-games/[uuid]/page.tsx
+++ app/shared-games/[uuid]/page.tsx
@@ -1,5 +1,17 @@
-const Page = () => {
-  return <div>공유게임</div>;
+'use client';
+
+import { use } from 'react';
+
+import GameDetailModal from '@/entities/shared-game/ui/game-detail/game-detail-modal';
+
+interface PageProps {
+  params: Promise<{ uuid: string }>;
+}
+
+const Page = ({ params }: PageProps) => {
+  const { uuid } = use(params);
+
+  return <GameDetailModal uuid={uuid} />;
 };
 
 export default Page;
