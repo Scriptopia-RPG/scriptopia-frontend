53bd2e09406aa44adc945f3340f9d30170e9eaab
allkong <allempti@gmail.com>
2025-10-08 07:26:41 +0900

    refactor: 불필요한 react 임포트 제거

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,4 @@
 import type { Preview } from '@storybook/nextjs';
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 
